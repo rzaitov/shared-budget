922f14c2d5dd647f8d4c0f11f2cd77d1b318ae09
Rustam Zaitov <rustam.zaitov@xamarin.com>
2016-07-27 02:52:43 +0300

    add ui prototype

---
+++ SharedBudget.iOS/NavigationController.cs
@@ -0,0 +1,14 @@
+using System;
+
+using UIKit;
+using Foundation;
+
+namespace SharedBudget.iOS
+{
+	public partial class NavigationController : UINavigationController
+	{
+		public NavigationController (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
---
+++ SharedBudget.iOS/TabBarController.cs
@@ -0,0 +1,14 @@
+using System;
+
+using UIKit;
+using Foundation;
+
+namespace SharedBudget.iOS
+{
+	public partial class TabBarController : UITabBarController
+	{
+		public TabBarController (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
