cfedc5b768f789931d8f18c5ac991745c316cb7b
Rustam Zaitov <rustam.zaitov@xamarin.com>
2016-07-26 21:51:32 +0300

    add a few model classes

---
+++ Core/Model.cs
@@ -0,0 +1,48 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+	public class Event
+	{
+		[BsonId]
+		public Guid Id { get; set; }
+
+		[BsonElement("name")]
+		public string Name { get; set; }
+
+		[BsonElement("people")]
+		public List<People> People { get; set; }
+	}
+
+	public class People
+	{
+		[BsonId]
+		public Guid Id { get; set; }
+
+		[BsonElement("name")]
+		public string Name { get; set; }
+
+		[BsonElement("expenses")]
+		public List<Expense> Expenses { get; set; }
+	}
+
+	public class Expense
+	{
+		[BsonId]
+		public Guid Id { get; set; }
+
+		[BsonElement("date")]
+		public DateTime Date { get; set; }
+
+		[BsonElement("amount")]
+		public decimal Amount { get; set; }
+
+		[BsonElement("targets")]
+		List<Guid> Targets { get; set; }
+	}
+}
---
+++ Core/SharedBugdetRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using MongoDB.Driver;
+
+namespace Core
+{
+	class SharedBugdetRepository
+	{
+		const string eventsCollectionName = "events";
+
+		readonly IMongoDatabase mongoDb;
+		readonly IMongoCollection<Event> eventsCollection;
+
+		public SharedBugdetRepository(IMongoDatabase mongoDb)
+		{
+			if (mongoDb == null)
+				throw new ArgumentNullException(nameof(mongoDb));
+			this.mongoDb = mongoDb;
+			eventsCollection = mongoDb.GetCollection<Event>(eventsCollectionName);
+		}
+
+		public void Add(Event newEvent)
+		{
+			eventsCollection.InsertOne(newEvent);
+		}
+	}
+}
